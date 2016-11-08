130081ecb65c3322b1e3577b2766e209579631e8
qizl <q@qizl.cn>
2016-11-09 00:48:56 +0800

    -

---
+++ Assets/Scripts/CubeControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            this.transform.Translate(0, 1f, 0);
+        if (Input.GetKey(KeyCode.A))
+            this.transform.Translate(-.01f, 0, 0);
+        if (Input.GetKey(KeyCode.D))
+            this.transform.Translate(.01f, 0, 0);
+    }
+}
